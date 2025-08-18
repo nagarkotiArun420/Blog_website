e873372b29051f38ab8c7be6bef401afa09bbb35
Arun Nagarkoti <nagarkotiarun420@gmail.com>
2025-08-18 14:47:50 +0545

    Categoy

--- blog_category/views.py
+++ blog_category/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog_category.models import Blogs
+
 # Create your views here.
-def posts_by_category(request, categroy_id):
-    return HttpResponse("post by category")
+def posts_by_category(request, category_id):
+    
+    #fetch the post that belongs to the category
+    posts = Blogs.objects.filter(status="published", category = category_id)
+    
+    context = {
+        'posts':posts
+    }
+    return render(request, 'posts_by_category.html',context)
